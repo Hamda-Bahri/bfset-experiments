293dfb5a65df0cd7193005f78900eb3353aefacc
Hamda-Bahri <79995231+Hamda-Bahri@users.noreply.github.com>
2025-11-13 23:01:29 +0100

    Add check_labels skeleton

---
+++ src/check_labels.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2025 Cognisoft and Hamda Ben Elbahri.
+# All rights reserved.
+#
+# This file is part of the BFSET experimental codebase.
+
+"""
+Visual check of YOLO annotations for BFSET (overlay beard bounding boxes).
+
+TODO:
+- Implement a function that loads an image + its YOLO label.
+- Draw the bounding box of the beard in red.
+- Show a few samples to validate annotation quality.
+"""
