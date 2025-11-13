ba188815bd0b1f5023902733e6908dcd01a1aa0f
Hamda-Bahri <79995231+Hamda-Bahri@users.noreply.github.com>
2025-11-13 22:56:41 +0100

    Add dataset_split skeleton

---
+++ src/dataset_split.py
@@ -0,0 +1,16 @@
+# Copyright (c) 2025 Cognisoft and Hamda Ben Elbahri.
+# All rights reserved.
+#
+# This file is part of the BFSET experimental codebase.
+# The BFSET dataset, name, and associated materials are proprietary and
+# not distributed through this repository.
+#
+# Unauthorized use, reproduction, or modification of this file is prohibited.
+
+"""
+Dataset split utilities for BFSET.
+
+TODO:
+- Implement train/val/test split.
+- Ensure images and YOLO labels are consistent.
+"""
