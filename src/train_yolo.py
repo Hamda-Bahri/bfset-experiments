9caeefe0d3ac8b9ac86546bfba136872c3669d49
Hamda-Bahri <79995231+Hamda-Bahri@users.noreply.github.com>
2025-11-13 23:05:05 +0100

    Add YOLO training script skeleton

---
+++ src/train_yolo.py
@@ -0,0 +1,20 @@
+# Copyright (c) 2025 Cognisoft and Hamda Ben Elbahri.
+# All rights reserved.
+#
+# This file is part of the BFSET experimental codebase.
+
+"""
+Training script for YOLO on BFSET.
+
+This script will mirror the logic of the Colab notebook:
+- Load bfset.yaml
+- Train a YOLO model (Ultralytics)
+- Save metrics and model weights
+"""
+
+# TODO:
+# from ultralytics import YOLO
+# def main():
+#     ...
+# if __name__ == "__main__":
+#     main()
