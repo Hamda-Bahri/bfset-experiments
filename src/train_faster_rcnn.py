852a1872ba692223559c6afdded50fbb1d760d77
Hamda-Bahri <79995231+Hamda-Bahri@users.noreply.github.com>
2025-11-13 23:06:55 +0100

    Add Faster R-CNN training script skeleton

---
+++ src/train_faster_rcnn.py
@@ -0,0 +1,14 @@
+# Copyright (c) 2025 Cognisoft and Hamda Ben Elbahri.
+# All rights reserved.
+#
+# This file is part of the BFSET experimental codebase.
+
+"""
+Training script for Faster R-CNN on BFSET.
+
+TODO:
+- Define a PyTorch dataset for BFSET.
+- Use torchvision.models.detection.fasterrcnn_resnet50_fpn.
+- Train on beard bounding boxes.
+- Export metrics and model checkpoints.
+"""
